70a4e11f90c60305a938509262ede685a607e577
bjd202 <44849201+bjd202@users.noreply.github.com>
2023-08-29 22:46:05 +0900

    수정 요청 기능 추가

--- app/quote/[id]/page.js
+++ app/quote/[id]/page.js
@@ -11,6 +11,12 @@ export default function QuotesDetail(){
     
     const [data, setData] = useState({})
 
+    const [isUpdateRequest, setIsUpdateRequest] = useState(false)
+
+    const [category, setCategory] = useState("")
+    const [content, setContent] = useState("")
+    const [description, setDescription] = useState("")
+
     useEffect(() => {
       const fetchData = async () => {
         const res = await fetch(`http://localhost:3001/quotes/${id}`, {
@@ -24,6 +30,9 @@ export default function QuotesDetail(){
         console.log(fetchData);
 
         setData(fetchData);
+
+        setCategory(fetchData.category);
+        setContent(fetchData.content);
       }
 
       fetchData();
@@ -34,29 +43,108 @@ export default function QuotesDetail(){
         router.push('/')
     }
 
+    const handleUpdateRequest = () => {
+      fetch("http://localhost:3001/update-request", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "quotes-id": data.id,
+          "category": category,
+          "content": content,
+          "description": description
+        })
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        alert("수정 요청이 정상적으로 처리되었습니다.");
+      })
+      .catch(err => console.log(err))
+    }
+
     return(
         <div>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>번호</th>
-                        <td>{data.id}</td>
-                    </tr>
-                    <tr>
-                        <th>분류</th>
-                        <td>{data.category}</td>
-                    </tr>
-                    <tr>
-                        <th>내용</th>
-                        <td>{data.content}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div>
+              <table>
+                  <tbody>
+                      <tr>
+                          <th>번호</th>
+                          <td>{data.id}</td>
+                      </tr>
+                      <tr>
+                          <th>분류</th>
+                          <td>{data.category}</td>
+                      </tr>
+                      <tr>
+                          <th>내용</th>
+                          <td>{data.content}</td>
+                      </tr>
+                  </tbody>
+              </table>
+            </div>
 
             <div>
-                <button>수정 요청</button>
+                <button onClick={() => setIsUpdateRequest(!isUpdateRequest)}>수정 요청</button>
                 <button onClick={handleList}>목록</button>
             </div>
+
+            {
+              isUpdateRequest ? (
+                <div>
+                  <div>
+                    <table>
+                      <tbody>
+                        <tr>
+                          <th>번호</th>
+                            <td>{data.id}</td>
+                          </tr>
+                          <tr>
+                            <th>분류</th>
+                            <td>
+                              <input 
+                                type="text" 
+                                value={category} 
+                                onChange={e => setCategory(e.target.value)} 
+                                style={{width: 300}}
+                              />
+                            </td>
+                          </tr>
+                          <tr>
+                            <th>내용</th>
+                            <td>
+                              <input 
+                                type="text" 
+                                value={content} 
+                                onChange={e => setContent(e.target.value)} 
+                                style={{width: 300}}
+                              />
+                            </td>
+                          </tr>
+                          <tr>
+                            <th>수정 요청 사유</th>
+                            <td>
+                              <textarea 
+                                value={description} 
+                                onChange={e => setDescription(e.target.value)}
+                                style={{width: 300, height: 100}}
+                              />
+                            </td>
+                          </tr>
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <div>
+                    <button onClick={handleUpdateRequest}>수정 요청</button>
+                  </div>
+
+                </div>
+              ) : null
+            }
+
+            
         </div>
     )
 }
